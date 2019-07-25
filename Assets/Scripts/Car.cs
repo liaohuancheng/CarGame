5dada91ec4185e5c540a094b51bd181cbfa41b8c
liaohuancheng <949104781@qq.com>
2019-07-25 10:39:30 +0800

    test

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    public WheelCollider fl;
+    public WheelCollider fr;
+
+    private float max_torque = 2000;
+    private float max_degree = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") == 0&& Input.GetAxis("Vertical")==0)
+        {
+            return;
+        }
+        this.fl.motorTorque = this.max_torque * Input.GetAxis("Vertical");
+        this.fr.motorTorque = this.max_torque * Input.GetAxis("Vertical");
+
+        this.fl.steerAngle = this.max_degree * Input.GetAxis("Horizontal");
+        this.fr.steerAngle = this.max_degree * Input.GetAxis("Horizontal");
+    }
+}
---
+++ Assets/Scripts/Wheels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheels : MonoBehaviour
+{
+    public WheelCollider wc;
+    public float degree = 0;//转动速度
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.degree = (this.wc.rpm * 360 / 60) * Time.deltaTime;
+        this.transform.rotation = this.wc.transform.rotation * Quaternion.Euler(this.degree, this.wc.steerAngle,0);
+    }
+}
